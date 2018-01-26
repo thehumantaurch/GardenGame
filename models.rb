fc6ec5e992cd2977fec5eef96142e6c1a71c6df2
Sarah Taurchini <sarah.taurchini@gmail.com>
2018-01-26 15:07:02 -0500

    add win state

--- models.rb
+++ models.rb
@@ -5,6 +5,7 @@ class Game
 	def initialize
 		@seed = Seed.new
     @weather = Weather.new
+    binding.pry
 	end
 
   def show_weather
@@ -39,7 +40,11 @@ class Game
       puts "It's a new day! Your plant is now #{@seed.height.to_s} cm tall"
       @weather = Weather.new
     else
-      "Sorry, you lost."
+      if @seed.height >= @seed.ideal_height
+        puts "Congratulations, you're a great gardener!"
+      else
+        puts "Sorry, you lost. Want to try again?"
+      end
     end
   end
 end
@@ -51,7 +56,8 @@ class Seed
   def initialize
     @ideal_temp = (60..80).to_a.sample
     @ideal_daily_water = (1..3).to_a.sample
-    @amt_sun_needed = (2..6).to_a.sample
+    @ideal_height = (10..20).to_a.sample
+    # @amt_daily_sun = (2..6).to_a.sample
     @height = 0
     @daily_water = 0
     @watered_today = false
@@ -59,17 +65,18 @@ class Seed
   end
 
   def calculate_daily_growth(weather)
-    if weather.precipitation == true && weather.sunlight == false
-      @daily_water += 2
-    elsif weather.precipitation == false && weather.sunlight == true
-      @amt_sun_needed -= 2
-    elsif weather.precipitation == false && weather.sunlight == false
-      @amt_sun_needed -= 1
-    else weather.precipitation == true && wweather.sunlight == true 
-      @daily_water += 1
-      @amt_sun_needed -= 1
-    end
+    # if weather.precipitation == true && weather.sunlight == false
+    #   @daily_water += 2
+    # elsif weather.precipitation == false && weather.sunlight == true
+    #   @amt_sun_needed -= 2
+    # elsif weather.precipitation == false && weather.sunlight == false
+    #   # @amt_sun_needed -= 1
+    # else weather.precipitation == true && wweather.sunlight == true 
+    #   @daily_water += 1
+    #   @amt_sun_needed -= 1
+    # end
 
+    @daily_water += weather.precipitation
     
     if @daily_water == 0
       water_ratio = 0
@@ -78,6 +85,7 @@ class Seed
     end
     temp_ratio = @ideal_temp.fdiv(weather.temperature)
     
+    @height += 1 if weather.sunlight != 0
     
     @height += 2 if water_ratio > 1
     @height += 1 if water_ratio < 1 && water_ratio > 0
@@ -89,15 +97,25 @@ end
 
 class Weather
 
-  PRECIPITATION = [ false, false, false, false, false, false, true, true, true, true ]
-  SUNLIGHT = [ false, false, false, true, true, true, true, true, true, true ]
-  attr_reader :precipitation, :sunlight
-  attr_accessor :temperature
+  # PRECIPITATION = [ false, false, false, false, false, false, true, true, true, true ]
+  PRECIPITATION = [ 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3 ]
+  # SUNLIGHT = [ false, false, false, true, true, true, true, true, true, true ]
+  SUNLIGHT = [ 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3 ]
+  attr_reader :precipitation
+  attr_accessor :temperature, :sunlight
 
 	def initialize
 		@temperature = (50..90).to_a.sample
-		@precipitation = PRECIPITATION.sample
-		@sunlight = SUNLIGHT.sample
+		@precipitation = PRECIPITATION.shuffle.pop
+		@sunlight = set_sunlight
 	end
+
+  def set_sunlight
+    if @precipitation == 3
+      @sunlight = 0
+    else
+      @sunlight = SUNLIGHT.shuffle.pop
+    end
+  end
 
 end
